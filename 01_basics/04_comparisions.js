a8d1f2b054e4374efb195cc79f918496383281f4
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-25 15:46:42 +0000

    comparisions done

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,15 @@
+//comparision different datatypes
+// console.log("2" >1)//true
+// console.log("02" >1)//true
+// console.log("2" >=1)//true
+// console.log("2" >true)//true
+// console.log("-2" >-1)//false
+// console.log(null >0)//false
+// console.log(null>=0)//true
+// console.log(null==0)//false
+// console.log(undefined >0)//false
+//  console.log(undefined>=0)//false
+//  console.log(undefined==0)//false
+
+//console.log("2"===1)//false it check data type on strict mode
+
