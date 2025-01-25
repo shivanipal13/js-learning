4a74c6ec4427ad315791cddf4f7ca0f5163d16f0
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-25 09:25:31 +0000

    conversion of datatypes done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,83 @@
+//Type Conversions
+// Converting Strings to Numbers
+// Converting Numbers to Strings
+// Converting Dates to Numbers
+// Converting Numbers to Dates
+// Converting Booleans to Numbers
+// Converting Numbers to Booleans
+// let score = 33//
+// let s = "33A";
+// console.log(typeof score)//number
+// console.log(typeof(score))//number
+// //let valuenInNumber=Number(score);//Converting in number
+// let valuenInNumber=Number(s);
+// //console.log(typeof valuenInNumber)//number
+
+// console.log(typeof valuenInNumber)//number 
+
+// console.log(typeof valuenInNumber);//converting into number but actual is not pure number
+// console.log(valuenInNumber)//NaN not a number 
+
+// let x="123";//string data
+//let convertNumber=Number(x);//it converts string into number
+//console.log(convertNumber);//123
+//console.log(typeof convertNumber)//number
+
+//let a=null
+// let convertNumber=Number(a);
+// console.log(typeof convertNumber)//number
+// console.log(convertNumber)//value is 0
+// let a=undefined
+// let convertNumber=Number(a);
+// console.log(typeof convertNumber)//number
+// console.log(convertNumber)//NaN
+
+// let a=true
+// let convertNumber=Number(a);
+// console.log(typeof convertNumber)//number
+// console.log(convertNumber)//1
+
+// let a=false
+// let convertNumber=Number(a);
+// console.log(typeof convertNumber)//number
+// console.log(convertNumber)//0
+
+
+// let a="a"
+// let convertNumber=Number(a);
+// console.log(typeof convertNumber)//number
+// console.log(convertNumber)//NaN
+
+//NOTE CONVERT TO NUMBER but type will show number
+//"33" => 33
+//"33abc" =>NaN
+//true => 1; false =>0
+//"abc" =>NaN
+
+// let isLoogedIn=1
+// let booleanConvert=Boolean(isLoogedIn)
+// console.log(booleanConvert);//true
+
+// let isLoogedIn=0
+// let booleanConvert=Boolean(isLoogedIn)
+// console.log(booleanConvert);//false
+
+
+// let isLoogedIn=""
+// let booleanConvert=Boolean(isLoogedIn)
+// console.log(booleanConvert);//false
+
+// let isLoogedIn="shivani"
+// let booleanConvert=Boolean(isLoogedIn)
+// console.log(booleanConvert);//true
+
+// let isLoogedIn=-0
+// let booleanConvert=Boolean(isLoogedIn)
+// console.log(booleanConvert);//true
+
+// -----Note---
+
+//1 => true; 0 ==>false
+//"shivani" => true; " " ==>false
+
+
