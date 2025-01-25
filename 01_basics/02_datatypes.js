89a88901d6feace090de79f774de227d347fe9d9
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-25 08:28:46 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+//aleart(3-3)//we are using node js , not browser 
+
+//Datatypes
+//non primitive type
+//string
+//number rang 2 to power of 53
+//boolean==>true/false
+//bigint ==> use trading stockmarket
+//null //standalone value /empty varible value //null type is object
+//symbol //unique
+//undefind //value not defined or assigned
+
+//non primitive type
+//object
+
+let age=20
+let x=null;
+
+console.log(typeof(age)); //type number
+console.log(typeof(null));// object
+
