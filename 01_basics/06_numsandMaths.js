d235235b412626770b1e23e14cb2049ceb4f11b9
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-27 10:36:47 +0000

    numbers done

---
+++ 01_basics/06_numsandMaths.js
@@ -0,0 +1,12 @@
+const score=400;
+console.log(score)
+
+const balance=new Number(100);//defind in object
+console.log(balance)
+console.log(balance.toFixed(2)); //100.00//return fixed number after decimal //Rounds the number to a specified number of decimal places and returns it as a string.
+
+///************************Math********************* */
+console.log(Math.abs(-3))//return absolute value 3
+console.log(Math.round(3.3))//3
+console.log(Math.ceil(3.2))//4
+console.log(Math.floor(3.9))//3
