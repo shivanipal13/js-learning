37f0080842e40e2221778ae667e78c3e78505f68
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-27 12:43:33 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+// //Arrays
+ const myaary=[1,2,3]
+// console.log(myaary)//[ 1, 2, 3 ]
+// console.log(typeof(myaary))//object
+// const myaar2=new Array(1,2,3,4)
+// console.log(myaar2)//[ 1, 2, 3, 4 ]
+// //array method
+
+// myaary.push(7)//append the new element at the end of an array
+// console.log(myaary)//[ 1, 2, 3, 7 ]
+// myaary.pop()//remove the last element
+// console.log(myaary)//[ 1, 2, 3 ]
+
+// myaary.unshift(9)//The unshift() method in JavaScript is used to add one or more elements to the beginning of an array. 
+// // It returns the new length of the array.
+
+
+//myaary.shift()//remove the first elements from the array.
+
+//slice, splice
+
+console.log(myaary.slice(1,3))//[ 2, 3 ]
+console.log(myaary)//origial aary [ 1, 2, 3 ]
+
+//console.clear()
