0b90cd2fd75a19957a1f0a5e01a2665aba05a3c0
Shivani Pal <154995668+shivanipal13@users.noreply.github.com>
2025-01-27 11:32:28 +0000

    Date done

---
+++ 01_basics/07_Date.js
@@ -0,0 +1,11 @@
+let mydate=new Date()//object
+//console.log(mydate)//2025-01-27T10:55:20.805Z
+
+//console.log(mydate.toString())//Mon Jan 27 2025 10:56:18 GMT+0000 (Coordinated Universal Time)
+
+//console.log(mydate.toDateString())//Mon Jan 27 2025
+//console.log(mydate.toISOString())//2025-01-27T11:01:02.032Z
+//console.log(mydate.toJSON())//2025-01-27T11:03:17.163Z
+//console.log(mydate.toLocaleDateString())//1/27/2025
+//console.log(mydate.getMonth())//
+//console.clear();
